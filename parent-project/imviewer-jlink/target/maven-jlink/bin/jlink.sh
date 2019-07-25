847d2c213f281300e239f37298c3b3e52817c6b0
haide <haide@DESKTOP-CG3KNFJ.Home>
2019-07-25 13:50:41 +0500

    Clean the target folder files

--- parent-project/imviewer-jlink/target/maven-jlink/bin/jlink.sh
+++
@@ -1,5 +0,0 @@
-export JAVA_HOME=C:\Program Files\Java\jdk-9
-JLINK=${JAVA_HOME}/bin/jlink
-JMODS=${JAVA_HOME}/jmods
-
-${JLINK} --module-path $JMODS:target/jmods --no-header-files --no-man-pages -G --compress=1 --add-modules=java.sql,java.desktop --output=target/jre 
